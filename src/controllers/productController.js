7ea5570b226ee8457c9019cbb4e1df28dac929e4
delficasarino <delficasarino@gmail.com>
2021-07-09 14:55:48 -0300

    json de imagenes y link en view

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,5 +2,6 @@ const product = require ("../models/product");
 
 module.exports = {
     index: (req, res)=>res.render("detalleMenu", {product: product.oneWithExtra(req.params.id)})
+    /* index: (req, res)=>res.send({product: product.oneWithExtra(req.params.id)}) */
 
 }
